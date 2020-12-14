bbed2d6a30d699acd07b71f64462b23ba57bbec7
Thomas Vickers <vickerst10@gmail.com>
2020-12-13 18:20:08 -0700

    Updating some funcationality to default home page with logo.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,28 @@
 <x-app-layout>
+
+
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
-            </div>
+    <div class="video-background" id="about">
+      <div class="video-wrap">
+        <div id="video">
+          <video id="bgvid" autoplay loop muted playsinline>
+            <source src="/img/videoplayback.webm" type="video/webm">
+          </video>
         </div>
+      </div>
     </div>
+
+    <div class="caption text-center">
+      <h1>Thomas Vickers</h1>
+      <!--
+      <a class="btn btn-outline-light btn-lg" href="about.php">Résumé</a>
+      -->
+    </div>
+
+
 </x-app-layout>
